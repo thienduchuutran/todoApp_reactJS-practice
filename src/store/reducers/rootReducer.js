55b3c5ca3c1d67f7862acc21d28834ffdac08207
Duc <thienductranhuu2784@gmail.com>
2024-04-06 18:19:00 -0400

    basic redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,12 +1,31 @@
 const initState = {
-    user: [
+    users: [
         {id: 1, name: 'Eric'},
-        {id: 2, name: 'Eric'},
-    ]
+        {id: 2, name: 'Eric IT'},
+        {id: 3  , name: 'Eric IT'},
+    ],
+    posts: []
 }
 const rootReducer = (state = initState, action) => {
 
-    return state;
+    switch(action.type){
+        case 'DELETE_USER':
+           let users = state.users
+           users = users.filter(item => item.id !== action.payload.id)
+           console.log(action)
+           return {
+                ...state, users
+            };
+        case 'CREATE_USER':
+            let id = Math.floor(Math.random()*10000)
+            let user = {id: id, name: `random ${id}`}
+            return {
+                ...state, users: [...state.users ,user]
+            } 
+        default:
+            return state;
+            }
+
 }
 
 export default rootReducer;
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 // import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 import Color from "../HOC/Color";
 import logo from '../../assets/img/logo.jpg'
+import { connect } from "react-redux";
 
 class Home extends React.Component{
     
@@ -11,18 +12,54 @@ class Home extends React.Component{
     //     }, 2000);
     // }
     
+    handleDeleteUser = (user) => {
+        this.props.deleteUserRedux(user)
+    }
+
+    handleCreateUser = () => {
+        this.props.addUserRedux()
+    }
 
     render(){
+        let listUser = this.props.dataRedux
         return(
             <>
+                
                 <div>Home</div>
                 <img style={{width: '300px', height: '300px', marginTop: '40px', borderRadius: '300px'}} src={logo}/>
+                <div>
+                    {listUser && listUser.length> 0 &&
+                    listUser.map((item, index) => {
+                        return (
+                            <div key={item.id}>
+                                {index + 1} - {item.name} <span onClick={() => this.handleDeleteUser(item)}><button >X</button></span>  
+                                  
+                                
+                            </div>
+                        )
+                    })
+                    }
+                    <span><button onClick={() => this.handleCreateUser()}>add new</button></span>
+                </div>
             </>
         )
     }
 }
 
 
+//we need these 2 functions and the connection below to complete connecting react app to redux
+const mapStateToProps = (state) => {
+    return {
+        dataRedux: state.users
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) => dispatch({type: 'DELETE_USER', payload: userDelete}),
+        addUserRedux: () => dispatch({type: 'CREATE_USER'}),
+    }
+}
 //when using withRouter (higher order component), it equips the component with more props
 // export default withRouter(Home);  
-export default Color(Home)  
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home))  
